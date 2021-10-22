905138bddfedcb97e81b9c2249e666fdf858bbe8
fioravante-joao <joao.fioravante@hotmail.com>
2021-10-22 11:12:16 -0300

    Remoção de usings desnecessarios

--- s3-project/Program.cs
+++ s3-project/Program.cs
@@ -1,14 +1,4 @@
-﻿using Amazon.Runtime;
-using Amazon;
-using Amazon.S3;
-using Amazon.S3.Model;
-
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Threading;
+﻿using System;
 
 namespace s3_project
 {
